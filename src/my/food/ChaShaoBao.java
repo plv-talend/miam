e831cbecc36b79a0974169179b72394f092b58bf
hwang-talend <hwang@talend.com>
2014-06-11 11:14:23 +0800

    hello word

--- src/my/food/ChaShaoBao.java
+++ src/my/food/ChaShaoBao.java
@@ -10,7 +10,7 @@ public class ChaShaoBao implements Food {
 	
 	/* Exercise 1: implement Constructor */
 	public ChaShaoBao(){
-		System.out.println();
+		System.out.println("hello word");
 	}
 	
 	@Override
