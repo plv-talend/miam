8df07a4930a6a28dfdee243cf4ef9efe875099e1
hwang-talend <hwang@talend.com>
2014-06-11 15:59:41 +0800

    remove3

--- src/my/food/ChaShaoBao.java
+++ src/my/food/ChaShaoBao.java
@@ -13,6 +13,10 @@ public class ChaShaoBao implements Food {
 	/* Exercise 1: implement Constructor */
 	public ChaShaoBao(){
 		
+		ingredients.add("chashaobao1");
+		ingredients.add("chashaobao2");
+		ingredients.add("chashaobao");
+		ingredients.add("chashaobao3");
 	}
 	
 	@Override
