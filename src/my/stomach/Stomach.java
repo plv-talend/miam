c2bfbb5ffafe125c7aa9ee0cecd0444f61cc9cb9
plv <plv@talend.com>
2014-06-11 15:36:10 +0800

    plv commit again

--- src/my/stomach/Stomach.java
+++ src/my/stomach/Stomach.java
@@ -20,6 +20,7 @@ public class Stomach {
 		// foodsInside.add(new Pizza()); // I like Pizza
 		foodsInside.add(new DanDanMian());// I like DanDanMian
 		foodsInside.add(new DanDanMian());// I like DanDanMian
+		foodsInside.add(new DanDanMian());// I like DanDanMian
 		// foodsInside.add(new Pizza()); // I like Pizza
 		// foodsInside.add(new Pizza()); // I like Pizza
 	}
